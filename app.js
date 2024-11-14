cb12463d7fa51f0e9c4690e08fe76aca8827a60a
felixMonti <175974086+felixMonti@users.noreply.github.com>
2024-11-14 13:12:21 -0300

    form site with validation completed

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
 const alert = document.querySelector(".password-alert");
 const password = document.querySelector("#password");
-const confirm = document.querySelector("#confirm");
+const confirmPassword = document.querySelector("#confirm-password");
 
-password.addEventListener("keyup", () => {
-    if (password.value !== confirm.value && confirm.value !== "") {
+password.addEventListener("input", () => {
+    // Remove custom validity message as the user types
+    password.setCustomValidity("");
+    confirmPassword.setCustomValidity("");
+    
+    // Check if passwords match as password input changes
+    if (password.value !== confirmPassword.value && confirmPassword.value !== "") {
+        confirmPassword.setCustomValidity("Passwords do not match");
         alert.style.display = "inline-block";
-        password.style.outlineColor = "red";
-        confirm.style.outlineColor = "red";
-    } else if (password.value === confirm.value && password.value !== "") {
+    } else {
+        confirmPassword.setCustomValidity("");
         alert.style.display = "none";
-        password.style.outlineColor = "green";
-        confirm.style.outlineColor = "green";
     }
 });
 
-confirm.addEventListener("keyup", () => {
-    if (password.value === confirm.value && password.value !== "") {
-        alert.style.display = "none";
-        password.style.outlineColor = "green";
-        confirm.style.outlineColor = "green";
-    } else if (confirm.value !== "") {
+confirmPassword.addEventListener("input", () => {
+    // Check if passwords match as confirmPassword input changes
+    if (password.value !== confirmPassword.value) {
+        confirmPassword.setCustomValidity("Passwords do not match");
+        confirmPassword.reportValidity();
         alert.style.display = "inline-block";
-        password.style.outlineColor = "red";
-        confirm.style.outlineColor = "red";
+    } else {
+        confirmPassword.setCustomValidity("");
+        alert.style.display = "none";
     }
 });
+
